3e8381115e684bb3b38179eeedd0eb2baa474f3f
Eran Hirsch <eranhirsch@gmail.com>
2023-05-25 17:46:19 +0300

    lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable unicorn/prefer-module */
+
+module.exports = {
+  // Code
+  "*.+(js|ts|jsx|tsx|mjs|cjs)": [
+    "eslint --cache --cache-location ./node_modules/.cache/eslint --fix",
+    "prettier --write",
+  ],
+  // Configs
+  "*.+(json|json5|yaml|yml)": "prettier --write",
+  // CSS
+  "*.+(css|scss|less)": "prettier --write",
+  // Web
+  "*.+(htm|html|svg)": "prettier --write",
+  // Markdown
+  "*.md": "prettier --write",
+  // GraphQL
+  "*.graphql": "prettier --write",
+};
