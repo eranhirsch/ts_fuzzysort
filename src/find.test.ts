75ba08fcb84bfa7d3aa3ba85559800034ae99da5
Eran Hirsch <eranhirsch@gmail.com>
2023-06-28 15:30:08 +0000

    doesn't work yet

--- src/find.test.ts
+++ src/find.test.ts
@@ -1,15 +1,17 @@
 import { UE4_FILES, URLS_AND_TITLES } from "../test/data";
 import { find } from "./find";
 
+const identity = <T>(x: T): T => x;
+
 // Ported from farzher/fuzzysort
 describe("legacy", () => {
   test("zom in URLS_AND_TITLES", () => {
-    const [firstResult] = find("zom", URLS_AND_TITLES, (text) => text);
+    const [firstResult] = find("zom", URLS_AND_TITLES, identity);
     expect(firstResult).toBe("jQuery Zoom");
   });
 
   test("cman in UE4_FILES", () => {
-    const result = find("cman", UE4_FILES, (text) => text).slice(0, 2);
+    const result = find("cman", UE4_FILES, identity).slice(0, 2);
     expect(result).toEqual(["CrowdManager.h", "CheatManager.h"]);
   });
 
@@ -17,9 +19,20 @@ describe("legacy", () => {
     const result = find(
       "adio",
       ["Audio.h", "AsyncTaskDownloadImage.h"],
-      (text) => text
+      identity
     );
     expect(result).length(1);
     expect(result).not.contain("AsyncTaskDownloadImage.h");
+  });
+
+  test("substring score order", () => {
+    const sortedMatches = [
+      "The Amazing Spider-Man",
+      "The Amazing Spider",
+      "The Amazing",
+      "The",
+    ];
+    const result = find("The Amazing Spider-Man", sortedMatches, identity);
+    expect(result).toEqual(sortedMatches);
   });
 });
