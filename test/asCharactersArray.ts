29f0d7669e44006027525f447288b18034bd4786
Eran Hirsch <eranhirsch@gmail.com>
2023-07-01 21:39:13 +0000

    nit

--- test/asCharactersArray.ts
+++ test/asCharactersArray.ts
@@ -1,7 +1,10 @@
 import { type NonEmptyArray } from "../src/utils/isNonEmpty";
 
 export function asCharactersArray(raw: ""): readonly [];
-export function asCharactersArray(raw: string): NonEmptyArray<string>;
+export function asCharactersArray(
+  raw: Exclude<string, "">,
+): NonEmptyArray<string>;
+export function asCharactersArray(raw: string): readonly string[];
 export function asCharactersArray(raw: string): readonly string[] {
   return [...raw.toLowerCase()];
 }
